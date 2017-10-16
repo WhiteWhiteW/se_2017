f6891ded6742ce722c85ab2b0d8004f1688797dd
WhiteWhiteW <wangerer1996@gmail.com>
2017-10-16 13:09:36 +0800

    menu 2.0 advanced

---
+++ lab4/linktable.h
@@ -0,0 +1,40 @@
+//
+//  linktable.h
+//  menu
+//
+//  Created by wyy on 2017/10/16.
+//  Copyright © 2017年 wyy. All rights reserved.
+//
+
+#ifndef _LINK_TABLE_H_
+#define _LINK_TABLE_H_
+#define SUCCESS 0
+#define FAILURE (-1)
+
+typedef struct LinkTableNode
+{
+    struct LinkTableNode *pNext;
+} tLinkTableNode;
+
+typedef struct LinkTable
+{
+    tLinkTableNode *pHead;
+    tLinkTableNode *pTail;
+    int SumOfNode;
+}tLinkTable;
+/**Create a LinkTable*/
+tLinkTable * CreateLinkTable();
+/**Delete a LinkTable*/
+int DeleteLinkTable(tLinkTable *pLinkTable);
+/** Add a LinktableNode to Linktable*/
+int AddLinkTableNode(tLinkTable *pLinkTable,tLinkTableNode *pNode);
+/** Delete a LinktableNode from Linktable*/
+int DelLinkTableNode(tLinkTable *pLinkTable,tLinkTableNode *pNode);
+/** Get LinktableHead*/
+tLinkTableNode *GetLinkTableHead(tLinkTable *pLinkTable);
+/** Get next LinktableNode*/
+tLinkTableNode *GetNextLinkTableNode(tLinkTable *pLinkTable,tLinkTableNode *pNode);
+#endif // _LINK_TABLE_H_
+
+/* linktable_h */
+
